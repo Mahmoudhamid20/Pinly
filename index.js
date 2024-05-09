f28fcfae813dea3363aacf9755ebda6d336e95ce
Mahmoud Hamid <80116197+Mahmoudhamid20@users.noreply.github.com>
2024-05-10 01:31:53 +0200

    Update: final version of pinly

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-let myLeads = []
+let myInput = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-    
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-console.log(leadsFromLocalStorage)
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myInput") )
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    renderLeads()
+    myInput = leadsFromLocalStorage
+    render(myInput)
 }
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    renderLeads()
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myInput.push(tabs[0].url)
+        localStorage.setItem("myInput", JSON.stringify(myInput) )
+        render(myInput)
+    })
 })
 
-function renderLeads() {
+function render(input) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < input.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${input[i]}'>
+                    ${input[i]}
                 </a>
             </li>
         `
     }
-    ulEl.innerHTML = listItems  
+    ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myInput = []
+    render(myInput)
+})
+
+inputBtn.addEventListener("click", function() {
+    myInput.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myInput", JSON.stringify(myInput) )
+    render(myInput)
+})
